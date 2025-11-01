0dfe931cc5122274cb468c6a4efc79f687f3a7eb
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2025-11-01 15:08:07 -0300

    test 7

--- api/api.js
+++ api/api.js
@@ -1,32 +1,32 @@
-async function getPublications() {
-  try {
-    const response = await fetch("http://localhost:3002/", {
-      cache: 'no-store', // Ensure fresh data on each request
-    });
+// async function getPublications() {
+//   try {
+//     const response = await fetch("http://localhost:3002/", {
+//       cache: 'no-store', // Ensure fresh data on each request
+//     });
     
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
     
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching publications:", error);
-    throw error;
-  }
-}
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching publications:", error);
+//     throw error;
+//   }
+// }
 
-async function createPublication(publication) {
-  //console.log(publication);
-  const response = await fetch("http://localhost:3002/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(publication),
-  });
-  const data = await response.json();
-  return data;
-}
+// async function createPublication(publication) {
+//   //console.log(publication);
+//   const response = await fetch("http://localhost:3002/", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(publication),
+//   });
+//   const data = await response.json();
+//   return data;
+// }
 
-export { getPublications, createPublication };
+// export { getPublications, createPublication };
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,25 @@
-import Publicaciones from "@/components/Publicaciones";
-import Link from "next/link";
+// import Publicaciones from "@/components/Publicaciones";
+// import Link from "next/link";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <div className="flex flex-col items-center justify-center h-screen">
+//       <h1 className="text-2xl font-bold mb-4">Ver Publicaciones</h1>
+//       <Link href="/crear" className="bg-blue-500 text-white px-4 py-2 rounded-md">Crear Publicación</Link>
+//       <div className="mt-4 bg-white p-4 rounded-lg shadow-md w-full max-w-2xl mx-auto">
+//           <Publicaciones />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Ver Publicaciones</h1>
-      <Link href="/crear" className="bg-blue-500 text-white px-4 py-2 rounded-md">Crear Publicación</Link>
-      <div className="mt-4 bg-white p-4 rounded-lg shadow-md w-full max-w-2xl mx-auto">
-          <Publicaciones />
-        </div>
-      </div>
+      <h1>Hello World</h1>
     </>
   );
-}
+} 
